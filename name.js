127ea53aee5df1fe7b821b5385ba928d49188063
VANH <161328703+vingtle@users.noreply.github.com>
2024-03-19 12:37:41 +0100

    Add files via upload

---
+++ name.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+const name = ['Vannes', 'Hanna', 'John', 'Alice'];
+console.log(name[0], name[1], name[2], name[3]);
+
+console.log(chalk.blue(name[0]), chalk.red(name[1]),chalk.white(name[2]), chalk.black(name[3]));
+
